01b2c272f3cf3edd4528d73b50f9b3c887572c5a
saaket123 <37738766+saaket123@users.noreply.github.com>
2018-12-11 15:56:51 +0530

    tutorial view added

--- welcome/views.py
+++ welcome/views.py
@@ -5,3 +5,7 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return render(request, 'welcome/index.html')
+
+
+def tutorial(request):
+    return HttpResponse('Heya Tutorial')
